6e80d85dc459961e368b83f5526708e8893c9d68
Dev Karwa <108174299+devkarwa28@users.noreply.github.com>
2023-02-06 12:09:29 +0530

    ok

--- src/global/Disclaimer.jsx
+++ src/global/Disclaimer.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import img1 from '../images/upi.svg'
+import img2 from "../images/chevron-right.svg"
 
 function Disclaimer(){
     return(
         <>
             <div class="disclaimer container-fluid">
-        <div class="container d-flex justify-content-between">
+        <div class="container2 d-lg-flex justify-content-between">
 
             <h1 class="color-767676 fw-normal jost font-16">Copyright 2022 © Jodhpuri Furniture - All Rights Reserved.</h1>
-            <img src={img1} alt=""/>
+            <img src={img1} alt="" className="sm-none"/>
+            <div className="d-flex d-lg-none justify-content-center">
+                            <button className="view-all-btn">
+                            Show More <img src={img2} />
+                            </button>
+            </div>
         </div>
      </div>
         </>
