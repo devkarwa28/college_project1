2fae8aab6dea114942e703404e96ddfb895349f6
Dev Karwa <108174299+devkarwa28@users.noreply.github.com>
2024-05-08 09:34:25 +0530

    ok

---
+++ src/Listing.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./global/Header";
+
+const function Listing(){
+    return(
+        <>
+        <Header/>
+        </>
+    )
+}
+
+export default Listing;
